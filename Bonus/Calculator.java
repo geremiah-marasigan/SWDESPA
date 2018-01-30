1801815c1e1d10a6fb2cd1e66cf6f138c6f36b83
kylesantiago <klsantiago424@gmail.com>
2018-01-30 20:57:15 +0800

    fixed I think

--- Bonus/Calculator.java
+++ Bonus/Calculator.java
@@ -1,27 +1,25 @@
 public class Calculator{
-	private Adder adder;
-	private Subtracter subtracter;
-	
+	private Calculate calcu_A, calcu_S;
 	public Calculator(){
-		adder = new Adder();
-		subtracter = new Subtracter();
-		
+		calcu_A = new Adder();
+		calcu_S = new Subtracter();
 	}
 
 	public void calculate(String[] input){
 		int answer = Integer.parseInt(input[0]);
 		for (int i = 1; i < input.length; i+=2) {
 			switch(input[i]){
-				case "+": answer = adder.Solve(answer, Integer.parseInt(input[i+1])); break;
-				case "-": answer = subtracter.Solve(answer, Integer.parseInt(input[i+1])); break;
+				case "+": answer = ((Adder)calcu_A).Solve(answer, Integer.parseInt(input[i+1])); break;
+				case "-": answer = ((Subtracter)calcu_S).Solve(answer, Integer.parseInt(input[i+1])); break;
 				case "/": int count = 0;
 						  while(answer >= Integer.parseInt(input[i+1])){
 							count++;
-						    answer = subtracter.Solve(answer, Integer.parseInt(input[i+1]));
+						    answer = ((Subtracter)calcu_S).Solve(answer, Integer.parseInt(input[i+1]));
 						  }	 answer = count; break;
+				case "*":
 				case "x": int temp = answer;
 						  for(int j = 1; j < Integer.parseInt(input[i+1]); j++)
-							answer = adder.Solve(answer, temp); break;
+							answer = ((Adder)calcu_A).Solve(answer, temp); break;
 			}
 		}
 		
@@ -29,3 +27,4 @@ public class Calculator{
 	}
 	
 }
+
