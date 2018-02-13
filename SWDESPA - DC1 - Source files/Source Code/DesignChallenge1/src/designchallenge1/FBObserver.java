c3a08cbcb5e2c210151fb40e8220bb4ab004cca5
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-02-13 17:35:33 +0800

    Events Now Show Only if Today

--- SWDESPA - DC1 - Source files/Source Code/DesignChallenge1/src/designchallenge1/FBObserver.java
+++ SWDESPA - DC1 - Source files/Source Code/DesignChallenge1/src/designchallenge1/FBObserver.java
@@ -14,16 +14,21 @@ import java.util.*;
 public class FBObserver extends NotificationObserver{
     private FBView fb;
     private ArrayList<Event> addedEvents;
+    private Calendar today;
     public FBObserver(FBView f, CalendarProgram cp){
         fb = f;
         owner = cp;
         addedEvents = new ArrayList<>();
+        today = Calendar.getInstance();
     }
     
     public void update(){
         for(Event e: owner.events){
-            if(!addedEvents.contains(e)){
-                fb.showNewEvent(e.getEvent(),e.getDate().getMonth(),e.getDate().getDate(),e.getDate().getYear(),e.getColor());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(e.getDate());
+            boolean isEventToday = calendar.get(Calendar.YEAR) == this.today.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == this.today.get(Calendar.DAY_OF_YEAR);
+            if(!addedEvents.contains(e) && isEventToday){
+                fb.showNewEvent(e.getEvent(),e.getDate().getMonth() + 1,e.getDate().getDate(),e.getDate().getYear() + 1900,e.getColor());
                 addedEvents.add(e);
             }
         }
--- SWDESPA - DC1 - Source files/Source Code/DesignChallenge1/src/designchallenge1/SMSObserver.java
+++ SWDESPA - DC1 - Source files/Source Code/DesignChallenge1/src/designchallenge1/SMSObserver.java
@@ -15,17 +15,20 @@ import java.util.*;
 public class SMSObserver extends NotificationObserver{
     private SMSView smsV;
     private ArrayList<Event> addedEvents;
+    private Calendar today;
     public SMSObserver(SMSView s, CalendarProgram cp){
         smsV = s;
         owner = cp;
         addedEvents = new ArrayList<>();
+        today = Calendar.getInstance();
     }
     
     public void update(){
         for(Event e: owner.events){
-            if(!addedEvents.contains(e)){
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(e.getDate());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(e.getDate());
+            boolean isEventToday = calendar.get(Calendar.YEAR) == this.today.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == this.today.get(Calendar.DAY_OF_YEAR);
+            if(!addedEvents.contains(e) && isEventToday){
                 SMS sms = new SMS(e.getEvent(), calendar, e.getColor());
                 smsV.sendSMS(sms);
                 addedEvents.add(e);
