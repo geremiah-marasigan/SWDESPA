cddee73fd4c04afa1b1589efe5f74675f4a8a2f5
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-03-07 20:48:52 +0800

    package

--- SWDESPA - DC2 - Source files/DesignChallenge2/DesignChallenge2/src/designchallenge2/DesignChallenge2.java
+++ SWDESPA - DC2 - Source files/DesignChallenge2/DesignChallenge2/src/designchallenge2/DesignChallenge2.java
@@ -1,13 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package designchallenge2;
+
 /**
  *
- * @author Arturo III
+ * @author dlsza
  */
-public class DesignChallenge1 {
+public class DesignChallenge2 {
 
     /**
      * @param args the command line arguments
@@ -16,4 +18,5 @@ public class DesignChallenge1 {
         // TODO code application logic here
         CalendarProgram cp = new CalendarProgram();
     }
+    
 }
